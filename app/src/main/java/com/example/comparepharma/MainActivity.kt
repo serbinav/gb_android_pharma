0004a1e9860e7b0eb20ef5a3c47d86731b7c5f49
andrey <a.serbin@litebox.ru>
2021-07-25 22:00:46 +0400

    Преобразовали MainActivity на Kotlin

--- app/src/main/java/com/example/comparepharma/MainActivity.kt
+++ app/src/main/java/com/example/comparepharma/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.example.comparepharma;
+package com.example.comparepharma
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
