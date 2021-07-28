fa97b753fee6278e163d449741ca586ccfa64e66
andrey <a.serbin@litebox.ru>
2021-07-28 21:14:15 +0400

    Инициализировали RecyclerView

--- app/src/main/java/com/example/comparepharma/MainActivity.kt
+++ app/src/main/java/com/example/comparepharma/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.comparepharma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.comparepharma.repository.RepositorySingle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerPharma = findViewById<RecyclerView>(R.id.recycler_view)
+        val adapter = PharmaAdapter(RepositorySingle)
+        recyclerPharma.adapter = adapter
     }
 }
