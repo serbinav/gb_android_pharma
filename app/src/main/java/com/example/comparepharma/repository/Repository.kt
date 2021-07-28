6bbacb9be6cd161893ee886dfe0eb6b99fbd623a
andrey <a.serbin@litebox.ru>
2021-07-28 22:28:55 +0400

    Тренировка в применении различных конструкций языка

--- app/src/main/java/com/example/comparepharma/repository/Repository.kt
+++ app/src/main/java/com/example/comparepharma/repository/Repository.kt
@@ -16,13 +16,20 @@ interface IRepository {
 //Singletone ближе всего static
 object RepositorySingle : IRepository {
 
+    private val pharma = Pharma("", "ИФИМОЛ", 90)
+
+    private val pharmaCopy = pharma.copy(price = 99, name = "ПАНАДОЛ")
+
     private val pharmas: List<Pharma> = listOf(
-        Pharma("", "ОТ ГРИППА И ПРОСТУДЫ ПОРОШОК СО ВКУСОМ ЛИМОНА", 440),
-        Pharma("", "САФИСТОН", 164),
-        Pharma("", "ТЕМПАЛГИН ТРИО", 157),
-        Pharma("", "ПАРАЦЕТАМОЛ", 58),
-        Pharma("", "ПАРАЦЕТАМОЛ", 71),
-        Pharma("", "ПАРАЦЕТАМОЛ", 21),
+        Pharma(name = "ОТ ГРИППА И ПРОСТУДЫ ПОРОШОК СО ВКУСОМ ЛИМОНА", price = 440),
+        Pharma(name = "САФИСТОН", price = 164),
+        Pharma(name = "ТЕМПАЛГИН ТРИО", price = 157),
+        Pharma(name = "ПАРАЦЕТАМОЛ", price = 58),
+        Pharma(name = "ПАРАЦЕТАМОЛ", price = 71),
+        Pharma(name = "ПАРАЦЕТАМОЛ", price = 21),
+        Pharma(name = "ПАРАЦЕТАМОЛ-ЭКО"),
+        pharma,
+        pharmaCopy
     )
 
     override fun getPharmas(): List<Pharma> {
