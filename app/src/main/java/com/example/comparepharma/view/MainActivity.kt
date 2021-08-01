4b3ada01cea974cff7ead77b755437b9d56723d0
andrey <a.serbin@litebox.ru>
2021-08-01 22:11:44 +0400

    ViewBinding для Activity

--- app/src/main/java/com/example/comparepharma/view/MainActivity.kt
+++ app/src/main/java/com/example/comparepharma/view/MainActivity.kt
@@ -6,17 +6,21 @@ import android.widget.Toast
 import com.example.comparepharma.data.Pharma
 import com.example.comparepharma.repository.RepositorySingle
 import com.google.android.material.button.MaterialButton
-import com.example.comparepharma.R
+import com.example.comparepharma.databinding.MainActivityBinding
 import java.util.*
 
 const val PARACETAMOL: String = "ПАРАЦЕТАМОЛ"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button = findViewById<MaterialButton>(R.id.button_search)
+        val button = findViewById<MaterialButton>(binding.buttonSearch.id)
 
         button.setOnClickListener { it ->
             val data: List<Pharma> = RepositorySingle.getPharmas()
@@ -43,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(binding.container.id, MainFragment.newInstance())
                 .commitNow()
         }
     }
