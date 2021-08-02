e6b4b5327100a753653d146e49bc410eb163e879
andrey <a.serbin@litebox.ru>
2021-08-02 20:08:21 +0400

    Перенес поле count из класса Medicament в Cost

--- app/src/main/java/com/example/comparepharma/model/data/Medicament.kt
+++ app/src/main/java/com/example/comparepharma/model/data/Medicament.kt
@@ -2,23 +2,5 @@ package com.example.comparepharma.model.data
 
 data class Medicament constructor(
     var img: String = "",
-    var name: String) {
-
-    var count: Int = 0
-        get() {
-            registrator()
-            return field
-        }
-        set(value) {
-            registrator()
-            field = value
-        }
-
-    constructor(img: String, name: String, count: Int) : this(img, name) {
-        this.count = count
-    }
-
-    fun registrator(): Unit{
-
-    }
-}
+    var name: String
+)
