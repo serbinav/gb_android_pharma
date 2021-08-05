c340866342b0a7787f10af3d2db9ca8f60f6387a
andrey <a.serbin@litebox.ru>
2021-08-05 18:51:17 +0400

    Обновил MainViewModel, добавил details_fragment и логотипы для аптек

--- app/src/main/java/com/example/comparepharma/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/comparepharma/viewmodel/MainViewModel.kt
@@ -7,35 +7,34 @@ import com.example.comparepharma.model.AppState
 import com.example.comparepharma.model.repository.RepositorySingleImpl
 import com.example.comparepharma.model.repository.Repository
 
-private const val WAIT_SMALL : Long = 1000
-private const val WAIT_LONG : Long = 2000
+private const val WAIT_LONG: Long = 3000
 
 class MainViewModel(private val repository: Repository = RepositorySingleImpl) :
     ViewModel() {
 
     private val liveDataToObserver: MutableLiveData<AppState> = MutableLiveData()
 
-    private var counter: Int = 0
-
     fun getDate(): LiveData<AppState> {
         return liveDataToObserver
     }
 
-    fun getPharmaFromLocal(){
-        liveDataToObserver.value = AppState.Loading
-        Thread{
-            Thread.sleep(WAIT_SMALL)
-            counter++
-            liveDataToObserver.postValue(AppState.Success(repository.getPharmaFromLocal()))
-        }.start()
-    }
+    fun getPharmaFromLocalAptekaRu() = getPharmaFromLocalSource(isAptekaRu = true)
 
-    fun getPharmaFromRemote(){
+    fun getPharmaFromLocalAptekaApril() = getPharmaFromLocalSource(isAptekaRu = false)
+
+    private fun getPharmaFromLocalSource(isAptekaRu: Boolean) {
         liveDataToObserver.value = AppState.Loading
-        Thread{
+        Thread {
             Thread.sleep(WAIT_LONG)
-            counter++
-            liveDataToObserver.postValue(AppState.Success(repository.getPharmaFromServer()))
+            liveDataToObserver.postValue(
+                AppState.Success(
+                    if (isAptekaRu) {
+                        repository.getPharmaFromLocalAptekaRu()
+                    } else {
+                        repository.getPharmaFromLocalAptekaApril()
+                    }
+                )
+            )
         }.start()
     }
 }
