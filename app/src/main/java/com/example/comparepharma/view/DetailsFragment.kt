391b6612c93ad01c5b1d5bd25fb52d261af5f3ac
andrey <a.serbin@litebox.ru>
2021-08-14 00:23:12 +0400

    Вынес работу с http в отдельный файл PharmaLoader

--- app/src/main/java/com/example/comparepharma/view/DetailsFragment.kt
+++ app/src/main/java/com/example/comparepharma/view/DetailsFragment.kt
@@ -2,9 +2,6 @@ package com.example.comparepharma.view
 
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,13 +10,6 @@ import androidx.fragment.app.Fragment
 import com.example.comparepharma.databinding.MainDetailsFragmentBinding
 import com.example.comparepharma.model.data.MedicineCost
 import com.example.comparepharma.model.dto.*
-import com.google.gson.Gson
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.MalformedURLException
-import java.net.URL
-import java.util.stream.Collectors
-import javax.net.ssl.HttpsURLConnection
 
 class DetailsFragment : Fragment() {
 
@@ -42,52 +32,25 @@ class DetailsFragment : Fragment() {
         medCostBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: MedicineCost()
         binding.main.hide()
         binding.loadingLayout.show()
-        loadMedicineCost()
+        val loader = PharmaLoader(onLoadListener, medCostBundle.medicament.id)
+        loader.loadMedicineCost()
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun loadMedicineCost() {
-        try {
-            val uri =
-                URL("https://web-api.apteka-april.ru/catalog/products?ID=${medCostBundle.medicament.id}&cityID=168660")
-            val handler = Handler(Looper.myLooper()!!)
-            Thread (
-                Runnable {
-                    lateinit var urlConnection: HttpsURLConnection
-                    try {
-                        urlConnection = uri.openConnection() as HttpsURLConnection
-                        urlConnection.requestMethod = "GET"
-                        urlConnection.readTimeout = 10000
-                        val bufferedReader =
-                            BufferedReader(InputStreamReader(urlConnection.inputStream))
-
-                        val aptekaAprilDTO: Array<SearchAprilDTO> =
-                            Gson().fromJson(getLines(bufferedReader), Array<SearchAprilDTO>::class.java)
-                        handler.post { displayPharma(aptekaAprilDTO[0]) }
-
-                    } catch (e: Exception) {
-                        Log.e("PHARMA", "FAIL CONNECTION", e)
-                        e.printStackTrace()
-                    } finally {
-                        urlConnection.disconnect()
-                    }
-            }).start()
-        } catch (ex: MalformedURLException) {
-            Log.e("PHARMA", "FAIL URI", ex)
-            ex.printStackTrace()
+    private val onLoadListener = object : PharmaLoader.PharmaLoaderListener{
+        override fun onLoaded(searchAprilDTO: SearchAprilDTO) {
+            displayPharma(searchAprilDTO)
         }
-    }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun getLines(reader: BufferedReader): String {
-        return reader.lines().collect(Collectors.joining("\n"))
+        override fun onFailed(throwable: Throwable) {
+            // Обработка ошибок
+        }
     }
 
     private fun displayPharma(pharmaDTO: SearchAprilDTO) {
         with(binding) {
             main.show()
             loadingLayout.hide()
-            pharmaDTO?.let { medicine ->
+            pharmaDTO.let { medicine ->
                 name.text = medicine.name
                 releaseForm.text = medicine.description.filter { it?.typeID == 19 }[0]?.description
                 dosage.text = medicine.properties.filter { it?.typeID == 20 }[0]?.name
