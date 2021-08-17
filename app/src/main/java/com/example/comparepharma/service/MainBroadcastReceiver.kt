48754676a4fa26654a1919b52acc7a6851591051
andrey <a.serbin@litebox.ru>
2021-08-17 23:05:57 +0400

    Сделал разные сообщения на включение и выключение режима AIRPLANE

--- app/src/main/java/com/example/comparepharma/service/MainBroadcastReceiver.kt
+++ app/src/main/java/com/example/comparepharma/service/MainBroadcastReceiver.kt
@@ -3,12 +3,24 @@ package com.example.comparepharma.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.provider.Settings
 import android.widget.Toast
 import com.example.comparepharma.R
 import com.example.comparepharma.view.showToast
 
 class MainBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
-        context.getString(R.string.broadcast_receiver_text).showToast(context, Toast.LENGTH_LONG)
+        if (Settings.Global.getInt(
+                context.contentResolver,
+                Settings.Global.AIRPLANE_MODE_ON,
+                0
+            ) != 0
+        ) {
+            context.getString(R.string.broadcast_receiver_airplane_on)
+                .showToast(context, Toast.LENGTH_LONG)
+        } else {
+            context.getString(R.string.broadcast_receiver_airplane_off)
+                .showToast(context, Toast.LENGTH_LONG)
+        }
     }
 }
