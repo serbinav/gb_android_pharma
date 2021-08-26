6746a4afcc9788896e81b8e5a299aa453e3cf911
andrey <a.serbin@litebox.ru>
2021-08-26 21:50:12 +0400

    Небольшие улучшения дизайна

--- app/src/main/java/com/example/comparepharma/view/MainActivity.kt
+++ app/src/main/java/com/example/comparepharma/view/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.comparepharma.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.comparepharma.R
-import com.google.android.material.button.MaterialButton
 import com.example.comparepharma.databinding.MainActivityBinding
 import kotlinx.android.synthetic.main.main_details_fragment.*
 import java.util.*
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val button: MaterialButton = binding.buttonSearch
+        val button: ImageButton = binding.buttonSearch
 
         button.setOnClickListener {
             getString(R.string.loading_text).showToast(this@MainActivity)
