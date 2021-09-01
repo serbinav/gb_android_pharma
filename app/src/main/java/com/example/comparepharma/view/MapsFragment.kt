b274888452bfd9e6758c8e7666b61d3feb98a709
andrey <a.serbin@litebox.ru>
2021-09-01 22:36:23 +0400

    Переделал MapsFragment на binding + переименовал fragment_maps в maps_fragment

--- app/src/main/java/com/example/comparepharma/view/MapsFragment.kt
+++ app/src/main/java/com/example/comparepharma/view/MapsFragment.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.comparepharma.R
+import com.example.comparepharma.databinding.MapsFragmentBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -15,6 +15,9 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
+
+    private var _binding: MapsFragmentBinding? = null
+    private val binding get() = _binding!!
 
     private val callback = OnMapReadyCallback { googleMap ->
         val ulyanovsk = LatLng(54.3282, 48.3866)
@@ -26,13 +29,19 @@ class MapsFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        _binding = MapsFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        val mapFragment = childFragmentManager.findFragmentById(binding.root.id) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
 
